5c86ac63acdfb723fae994e36fce0458d2f75ff7
hamdi Yıldız <hamdiyildiz06@gmail.com>
2021-02-14 23:30:43 +0300

    Settings verilerini middlaware sayesinde sitede kullanılacak şekilde ayarladım 16

---
+++ app/Http/Middleware/Share.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Setting;
+use Closure;
+use Illuminate\Support\Facades\View;
+
+class Share
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $data = [];
+        $settings = Setting::all();
+
+        foreach ($settings as $setting){
+            $settingArr[$setting->key] = $setting->value;
+        }
+
+        $data["settings"] =  $settingArr;
+
+        View::share($data);
+        return $next($request);
+    }
+}
